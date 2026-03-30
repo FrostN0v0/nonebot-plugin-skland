60a9633d1b53f62a98bec472363047bb08e0ba1b
FrostN0v0 <1614591760@qq.com>
2026-03-30 09:26:20 +0800

    🐛 修正 alc shortcuts 缓存行为，避免影响其他插件

--- nonebot_plugin_skland/hook.py
+++ nonebot_plugin_skland/hook.py
@@ -8,6 +8,7 @@ from .data_source import gacha_table_data, ef_gacha_pool_data
 
 driver = get_driver()
 shortcut_cache = CACHE_DIR / "shortcut.db"
+from .matcher import skland, skland_command
 
 
 @driver.on_startup
@@ -22,8 +23,35 @@ async def startup():
     except RequestException as e:
         logger.error(f"终末地卡池数据加载失败: {e}")
     logger.debug("Endfield gacha pool table data loaded")
-    command_manager.load_cache(shortcut_cache)
+    command_manager.load_cache(shortcut_cache, command=skland_command)
     logger.debug("Skland shortcuts cache loaded")
+    skland.shortcut("森空岛绑定", {"command": "skland bind", "fuzzy": True, "prefix": True})
+    skland.shortcut("扫码绑定", {"command": "skland qrcode", "fuzzy": False, "prefix": True})
+    skland.shortcut("森空岛解绑", {"command": "skland unbind", "fuzzy": False, "prefix": True})
+    skland.shortcut("明日方舟签到", {"command": "skland arksign sign --all", "fuzzy": False, "prefix": True})
+    skland.shortcut("签到详情", {"command": "skland arksign status", "fuzzy": False, "prefix": True})
+    skland.shortcut("全体签到", {"command": "skland arksign all", "fuzzy": False, "prefix": True})
+    skland.shortcut("全体签到详情", {"command": "skland arksign status --all", "fuzzy": False, "prefix": True})
+    skland.shortcut("界园肉鸽", {"command": "skland rogue --topic 界园", "fuzzy": True, "prefix": True})
+    skland.shortcut("萨卡兹肉鸽", {"command": "skland rogue --topic 萨卡兹", "fuzzy": True, "prefix": True})
+    skland.shortcut("萨米肉鸽", {"command": "skland rogue --topic 萨米", "fuzzy": True, "prefix": True})
+    skland.shortcut("水月肉鸽", {"command": "skland rogue --topic 水月", "fuzzy": True, "prefix": True})
+    skland.shortcut("傀影肉鸽", {"command": "skland rogue --topic 傀影", "fuzzy": True, "prefix": True})
+    skland.shortcut("角色更新", {"command": "skland char update", "fuzzy": False, "prefix": True})
+    skland.shortcut("全体角色更新", {"command": "skland char update --all", "fuzzy": False, "prefix": True})
+    skland.shortcut("资源更新", {"command": "skland sync", "fuzzy": True, "prefix": True})
+    skland.shortcut("战绩详情", {"command": "skland rginfo", "fuzzy": True, "prefix": True})
+    skland.shortcut("收藏战绩详情", {"command": "skland rginfo -f", "fuzzy": True, "prefix": True})
+    skland.shortcut("方舟抽卡记录", {"command": "skland gacha -l 3", "fuzzy": True, "prefix": True})
+    skland.shortcut("导入抽卡记录", {"command": "skland import", "fuzzy": True, "prefix": True})
+    skland.shortcut("终末地签到", {"command": "skland efsign sign --all", "fuzzy": False, "prefix": True})
+    skland.shortcut("终末地全体签到", {"command": "skland efsign all", "fuzzy": False, "prefix": True})
+    skland.shortcut("终末地签到详情", {"command": "skland efsign status", "fuzzy": False, "prefix": True})
+    skland.shortcut("终末地全体签到详情", {"command": "skland efsign status --all", "fuzzy": False, "prefix": True})
+    skland.shortcut(r"(ef|zmd)", {"command": "skland efcard", "fuzzy": True, "prefix": True})
+    skland.shortcut("终末地抽卡记录", {"command": "skland efgacha", "fuzzy": True, "prefix": True})
+    skland.shortcut("终末地抽卡更新", {"command": "skland efgacha -u", "fuzzy": True, "prefix": True})
+
     if config.check_res_update:
         try:
             await download_img_resource(force=False, update=False)
@@ -33,5 +61,5 @@ async def startup():
 
 @driver.on_shutdown
 async def shutdown():
-    command_manager.dump_cache(shortcut_cache)
+    command_manager.dump_cache(shortcut_cache, command=skland_command)
     logger.debug("Skland shortcuts cache dumped")
